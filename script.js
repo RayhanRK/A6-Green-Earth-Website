4ab6a08a68422121536cf573f0aa15062384304c
RayhanRK <m.rayhankhan01@gmail.com>
2025-09-08 15:53:53 +0600

    HTML code completed with some dynamic js functionality

---
+++ script.js
@@ -0,0 +1,151 @@
+// cart list
+let cartList = [];
+const cartDiv = document.getElementById("cartContainer");
+const plantCards = document.getElementById("plantCards");
+
+// spinner functionality
+const spinner = document.getElementById("spinnerLoader");
+
+const spinnerLoader = (loadingState) => {
+  if (loadingState) {
+    spinner.classList.remove("hidden");
+    plantCards.classList.add("hidden"); 
+  } else {
+    spinner.classList.add("hidden");
+    plantCards.classList.remove("hidden");
+  }
+};
+
+// Load Categories
+const loadAllCategory = async () => {
+  const url = "https://openapi.programming-hero.com/api/categories";
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    showAllCategory(data.categories);
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+  }
+};
+
+const showAllCategory = (categories) => {
+  const catList = document.getElementById("categoryList");
+  catList.innerHTML = ""; // clear old
+
+  // append only fetched categories
+  categories.forEach((category) => {
+    catList.innerHTML += `
+      <li onclick="loadPlantsByCategory('${category.id}')" 
+          class="px-3 py-2 cursor-pointer rounded duration-300 hover:bg-[#15803d] hover:text-white">
+          ${category.category_name}
+      </li>
+    `;
+  });
+};
+
+
+// Load Plants by Category
+const loadPlantsByCategory = async (id) => {
+  spinnerLoader(true);
+  const url = `https://openapi.programming-hero.com/api/category/${id}`;
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    showPlantsByCategory(data.plants, id);
+  } catch (error) {
+    console.error("Error fetching category plants:", error);
+    spinnerLoader(false);
+  }
+};
+
+const showPlantsByCategory = (plants, id) => {
+  removeActive();
+  // highlight active category
+  const activeItem = document.getElementById("all" + id);
+  if (activeItem) {
+    activeItem.classList.add("bg-[#15803d]", "text-white");
+  }
+
+  plantCards.innerHTML = "";
+  plants.forEach((plant) => {
+    plantCards.innerHTML += `
+      <div class="card bg-base-100 w-full shadow-sm">
+        <figure class="px-4 pt-4">
+          <img src="${plant.image}" alt="${plant.name}"
+              class="rounded-xl w-full h-50 object-cover" />
+        </figure>
+        <div class="card-body pb-4">
+          <h2 class="card-title cursor-pointer" onclick="loadPlantDetail('${plant.id}')">${plant.name}</h2>
+          <p>${plant.description}</p>
+          <div class="flex justify-between items-center">
+            <div class="rounded-full bg-[#dcfce7] text-[#15803d] py-1 px-3 font-semibold">${plant.category}</div>
+            <div class="font-semibold">৳${plant.price}</div>
+          </div>
+        </div>
+        <div class="pb-4 px-4">
+          <button class="btn bg-[#15803d] btn-block rounded-full text-white">Add to Cart</button>
+        </div>
+      </div>
+    `;
+  });
+  spinnerLoader(false);
+};
+
+// Load All Plants
+
+const loadAllPlants = async () => {
+  spinnerLoader(true);
+  const url = "https://openapi.programming-hero.com/api/plants";
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    showAllPlants(data.plants);
+  } catch (error) {
+    console.error("Error fetching plants:", error);
+    spinnerLoader(false);
+  }
+};
+
+const showAllPlants = (plants) => {
+  removeActive();
+
+  // highlight All Trees button
+  const allTreeBtn = document.getElementById("allTree");
+  if (allTreeBtn) {
+    allTreeBtn.classList.add("bg-[#15803d]", "text-white");
+  }
+
+  plantCards.innerHTML = "";
+  plants.forEach((plant) => {
+    plantCards.innerHTML += `
+      <div class="card bg-base-100 w-full shadow-sm">
+        <figure class="px-4 pt-4">
+          <img src="${plant.image}" alt="${plant.name}"
+              class="rounded-xl w-full h-50 object-cover" />
+        </figure>
+        <div class="card-body pb-4">
+          <h2 class="card-title cursor-pointer" onclick="loadPlantDetail('${plant.id}')">${plant.name}</h2>
+          <p>${plant.description}</p>
+          <div class="flex justify-between items-center">
+            <div class="rounded-full bg-[#dcfce7] text-[#15803d] py-1 px-3 font-semibold">${plant.category}</div>
+            <div class="font-semibold">৳${plant.price}</div>
+          </div>
+        </div>
+        <div class="pb-4 px-4">
+          <button class="btn bg-[#15803d] btn-block rounded-full text-white">Add to Cart</button>
+        </div>
+      </div>
+    `;
+  });
+  spinnerLoader(false);
+};
+
+
+const removeActive = () => {
+  const buttons = document.querySelectorAll("#categoryList li, #allTree");
+  buttons.forEach((btn) => btn.classList.remove("bg-[#15803d]", "text-white"));
+};
+
+
+loadAllCategory();
+loadAllPlants();
